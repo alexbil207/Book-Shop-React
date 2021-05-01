d16b01174086ae33dafa1d964c6392e177883978
AlexBil <alexbil207@gmail.com>
2021-05-01 18:34:02 +0300

    storage bug

--- pages/BookApp.jsx
+++ pages/BookApp.jsx
@@ -7,6 +7,10 @@ export class BookApp extends React.Component {
     state = {
         books: [],
         selectedBook: null,
+        filterBy: {
+            value: '',
+            price: '',
+        }
     }
 
     componentDidMount() {
@@ -21,14 +25,22 @@ export class BookApp extends React.Component {
     setSelectedBook = (book) => {
         this.setState({ selectedBook: book })
     }
+    setFilterBy = ({ value, price }) => {
+        this.setState({
+            filterBy: {
+                value,
+                price,
+            }
+        })
 
+    }
 
     render() {
         const { books, selectedBook } = this.state;
         if (!books) return <h1>Loading......</h1>
         return (
             <React.Fragment>
-                {!selectedBook && <BookFilter />}
+                {!selectedBook && <BookFilter setFilterBy={this.setFilterBy} />}
                 {!selectedBook && <BookList books={books} setSelectedBook={this.setSelectedBook} />}
                 {selectedBook && <BookDetails book={selectedBook} setSelectedBook={this.setSelectedBook} />}
             </React.Fragment>
